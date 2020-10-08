fd382730a42fb424da54d8ee9994f717edd10e66
Andreas Z. Singer <azsinger@bitbucket.org>
2020-10-08 16:00:35 -0400

    Added additional comments

--- TISimulator/src/GameManager.java
+++ TISimulator/src/GameManager.java
@@ -26,6 +26,8 @@ public class GameManager
 	// Method to determine number of players and their names from the user
 	// Currently does not handle any exceptions
 	// For now this will work as though a single terminal is being used
+	// Might make a function/class for any time there is user input
+	// Might add option for user to input a list of names
 	public void whosPlaying() throws IOException{
 		// Make sure number of players does not exceed cap for the game
 		BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
@@ -34,22 +36,27 @@ public class GameManager
 		while (true) {
 			String s = inputStream.readLine();
 			
+			// Check if we have reached max number of players
 			if (this.playerNames.size() == this.playerCap) {
 				System.out.println("Maximum number of players reached.");
 				break;
 			}
+			// Check if user is done
 			else if (s.equals("done")) {
 				String output = String.format("The current players are %s \nIs this correct? [Y/n]", this.playerNames);
 				System.out.println(output);
-				String reply = inputStream.readLine();
+				String reply = inputStream.readLine(); // Check how they replied to the above question
+				// If the user said yes, we are done
 				if (reply.equals("Y")) {
 					break;
 				}
+				// If they said no, print prompt and continue
 				else if (reply.equals("n")) {
 					System.out.println("Please add the remaining players:");
 					continue;
 				}
 			}
+			// Default is to add the input string as a user name
 			else {
 				this.playerNames.add(s); // Add the input line to player names
 			}
